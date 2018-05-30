fc6dd56a64dac4332c258c8c5ec4d8ab6242f3ce
Clint Hall <bclinthall@gmail.com>
2018-05-29 20:14:39 -0400

    initial commit'

---
+++ promptcolors.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+# customize prompt
+GRAY='\[\033[1;30m\]'
+LIGHT_RED='\[\033[1;31m\]'
+LIGHT_GREEN='\[\033[1;32m\]'
+LIGHT_YELLOW='\[\033[1;33m\]'
+LIGHT_BLUE='\[\033[1;34m\]'
+LIGHT_PURPLE='\[\033[1;35m\]'
+LIGHT_CYAN='\[\033[1;36m\]'
+WHITE='\[\033[1;37m\]'
+CLEAR_COLOR='\[\033[0m\]'
